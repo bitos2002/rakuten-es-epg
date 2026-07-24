55dd5b27adbc0dfcdbf4238c3df2e8d8d674a41f
oscar <bitos2002@gmail.com>
2026-07-24 14:22:22 +0200

    First Update Fork for ES Rakuten

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta, time, timezone
 import pytz
 import unicodedata
 
-tz = pytz.timezone('Europe/London')
+tz = pytz.timezone('Europe/Madrid')
 
 
 # From https://stackoverflow.com/questions/4324790/removing-control-characters-from-a-string-in-python
@@ -27,7 +27,7 @@ Make the channels and programmes into something readable by XMLTV
     :param programmes: The list of programmes to be generated
     :return: A sequence of bytes for XML
     """
-    # Timezones since UK has daylight savings
+    # Timezones since ES has daylight savings
     dt_format = '%Y%m%d%H%M%S %z'
 
     # Helper: accept either an epoch (int/float) or a timezone-aware datetime
@@ -63,23 +63,23 @@ Make the channels and programmes into something readable by XMLTV
         programme.set("stop", end_time)
 
         title = etree.SubElement(programme, "title")
-        title.set('lang', 'en')
+        title.set('lang', 'es')
         title.text = pr.get("title")
 
         if pr.get("subtitle") is not None:
             subtitle = etree.SubElement(programme, "sub-title")
-            subtitle.set('lang', 'en')
+            subtitle.set('lang', 'es')
             subtitle.text = remove_control_characters(pr.get("subtitle"))
 
         if pr.get('description') is not None:
             description = etree.SubElement(programme, "desc")
-            description.set('lang', 'en')
+            description.set('lang', 'es')
             description.text = remove_control_characters(pr.get("description"))
 
         if pr.get('tags') is not None:
             if len(pr.get('tags')) > 0:
                 category = etree.SubElement(programme, "category")
-                category.set('lang', 'en')
+                category.set('lang', 'es')
                 for tag in pr.get('tags'):
                     category.text = tag.get("name")
 
@@ -88,15 +88,23 @@ Make the channels and programmes into something readable by XMLTV
 
 days = get_days()
 
-url_string = (f"classification_id=18&device_identifier=web"
-              f"&device_stream_audio_quality=2.0&device_stream_hdr_type=NONE&device_stream_video_quality=FHD"
-              f"&epg_duration_minutes=360"
-              f"&epg_ends_at={days[-1].strftime('%Y-%m-%dT%H:%M:%S.000Z')}"
-              f"&epg_ends_at_timestamp={days[-1].timestamp()}"
-              f"&epg_starts_at={days[0].strftime('%Y-%m-%dT%H:%M:%S.000Z')}"
-              f"&epg_starts_at_timestamp={days[0].timestamp()}"
-              f"&locale=en&market_code=uk"
-              f"&per_page=250")
+
+url_string = (
+    f"classification_id=5"
+    f"&device_identifier=web"
+    f"&device_stream_audio_quality=2.0"
+    f"&device_stream_hdr_type=NONE"
+    f"&device_stream_video_quality=FHD"
+    f"&epg_duration_minutes=360"
+    f"&epg_ends_at={days[-1].strftime('%Y-%m-%dT%H:%M:%S.000Z')}"
+    f"&epg_ends_at_timestamp={days[-1].timestamp()}"
+    f"&epg_starts_at={days[0].strftime('%Y-%m-%dT%H:%M:%S.000Z')}"
+    f"&epg_starts_at_timestamp={days[0].timestamp()}"
+    f"&locale=es"
+    f"&market_code=es"
+    f"&per_page=250"
+)
+
 
 url = "https://gizmo.rakuten.tv/v3/live_channels?" + url_string.replace(":", "%3A")
 print("Grabbing data")
